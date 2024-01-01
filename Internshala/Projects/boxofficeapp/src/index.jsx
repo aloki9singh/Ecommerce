2802333d47b2fad2b46baeb9bbb5266a6c5cc0c8
ALOK SINGH <107742899+aloki9singh@users.noreply.github.com>
2024-01-01 23:43:55 +0530

    index.js

--- Internshala/Projects/boxofficeapp/src/index.jsx
+++ Internshala/Projects/boxofficeapp/src/index.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
+// import * as ServiceWorkerRegistration from "./serviceWorkerRegistration"
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
- 
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  
+  // <BrowserRouter>
+  <HashRouter>
+    <App />
+  </HashRouter>
+
+  // </BrowserRouter>
 );
+
+// if (process.env.NODE_ENV == "production") {
+//   ServiceWorkerRegistration.register();
+// }
