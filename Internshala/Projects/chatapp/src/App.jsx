1c61e8e8e323ead764b4a213d4a0bfebf3c40ddd
ALOK SINGH <107742899+aloki9singh@users.noreply.github.com>
2024-01-02 23:25:52 +0530

    chatapp

--- Internshala/Projects/chatapp/src/App.jsx
+++ Internshala/Projects/chatapp/src/App.jsx
@@ -2,7 +2,6 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
-hi
 function App() {
   return (
     <BrowserRouter>
