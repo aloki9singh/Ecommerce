69eda41d1aaa45fca18baeaf4e81f77b2157453c
ALOK SINGH <107742899+aloki9singh@users.noreply.github.com>
2024-02-28 00:53:55 +0530

    St

--- Internshala/Projects/chatapp/src/App.jsx
+++ Internshala/Projects/chatapp/src/App.jsx
@@ -6,8 +6,6 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        
-        
         <Route path="/signin">SignIn</Route>
         <Route path="/">Home</Route>
       </Switch>
